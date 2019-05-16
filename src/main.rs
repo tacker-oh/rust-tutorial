866a18b8556f71a713104811721bc6432ee00f2f
H.T <tk04m9@gmail.com>
2019-05-16 15:15:46 +0900

    Guess Game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate rand;
+
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+    
+    let secret_num = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is {}", secret_num);
+
+    println!("Please input your guess.");
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
